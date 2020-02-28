04b8be6d7efaafc355b739fbfbd4cc5e002bf254
Juan Camilo Perez <pentre@Juans-MacBook-Pro.local>
2020-02-27 22:31:43 -0500

    Add client

--- www/www/settings.py
+++ www/www/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
+    'client',
     'rest_framework',
     'corsheaders',
 ]
--- www/www/urls.py
+++ www/www/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1.0/', include('user.urls'))
+    path('', include('user.urls')),
+    path('', include('client.urls'))
 ]
