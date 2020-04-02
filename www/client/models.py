e088eab0ef620b01961d8d95f774cf1c0e290f34
sofia <dnavas@truora.com>
2020-04-02 18:41:34 -0500

    Add clien name

--- www/client/models.py
+++ www/client/models.py
@@ -8,6 +8,7 @@ class Client(models.Model):
     client_id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
     document_id = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
     phone_number = models.CharField(max_length=15)
     address = models.CharField(max_length=30)
     active = models.BooleanField()
