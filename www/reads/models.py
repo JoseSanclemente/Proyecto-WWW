19e7c05f822bf93900e75a3e33b907bf29fb835d
sofia <dnavas@truora.com>
2020-04-02 13:54:51 -0500

    Fix reader fields

--- www/reads/models.py
+++ www/reads/models.py
@@ -5,7 +5,7 @@ import uuid
 class Reads(models.Model):
     reads_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     meter = models.ForeignKey('meter.Meter', null=True, on_delete=models.SET_NULL)
-    active_value = models.BooleanField()
-    reactive_value = models.BooleanField()
+    active_value = models.FloatField()
+    reactive_value = models.FloatField()
     date = models.DateField()
 
