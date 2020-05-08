5668bc786aff1076304cf83d549259df5ef67d86
Cristian Perafan <crystian.perafan@correounivalle.edu.com>
2020-05-08 00:41:18 -0500

    Login and captcha added

---
+++ dashboard/src/views/Login.vue
@@ -0,0 +1,128 @@
+<template>
+  <div>
+    <form novalidate class="md-layout" @submit.prevent="validateUser">
+      <md-card class="md-layout-item md-size-50 md-small-size-100">
+        <md-card-header>
+          <div class="md-title">Sign in</div>
+        </md-card-header>
+
+        <md-card-content>
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-100">
+              <md-field :class="getValidationClass('userName')">
+                <label for="user-name">User name</label>
+                <md-input name="user-name" id="first-name" autocomplete="given-name" v-model="form.firstName" :disabled="sending" />
+                <span class="md-error" v-if="!$v.form.firstName.required">The first name is required</span>
+                <span class="md-error" v-else-if="!$v.form.firstName.minlength">Invalid first name</span>
+              </md-field>
+            </div>
+
+            <div class="md-layout-item md-small-size-100">
+                <md-field>
+                <label>Password toggle</label>
+                <md-input v-model="password" type="password"></md-input>
+                </md-field>
+            </div>
+          </div>
+        </md-card-content>
+
+        <md-card-content>
+            <vue-recaptcha sitekey="6LcGM_QUAAAAAMS5nL-7j3R9hUWj-Ku0MlEFqxLg">
+                <button>Click me</button>
+            </vue-recaptcha>
+        </md-card-content>
+
+        <md-progress-bar md-mode="indeterminate" v-if="sending" />
+
+        <md-card-actions>
+          <md-button type="submit" class="md-primary" :disabled="sending">Sign in</md-button>
+        </md-card-actions>
+      </md-card>
+
+      <md-snackbar :md-active.sync="userSaved">The user {{ lastUser }} was saved with success!</md-snackbar>
+    </form>
+  </div>
+</template>
+
+<script>
+  import VueRecaptcha from 'vue-recaptcha';
+  import { validationMixin } from 'vuelidate'
+  import {
+    required,
+    email,
+    minLength,
+  } from 'vuelidate/lib/validators'
+
+  export default {
+    name: 'FormValidation',
+    mixins: [validationMixin],
+    data: () => ({
+      form: {
+        userName: null,
+        password: null,
+      },
+      userSaved: false,
+      sending: false,
+      lastUser: null
+    }),
+    components: { 
+        VueRecaptcha 
+    },
+    validations: {
+      form: {
+        firstName: {
+          required,
+          minLength: minLength(3)
+        },
+        email: {
+          required,
+          email
+        }
+      }
+    },
+    methods: {
+      getValidationClass (fieldName) {
+        const field = this.$v.form[fieldName]
+
+        if (field) {
+          return {
+            'md-invalid': field.$invalid && field.$dirty
+          }
+        }
+      },
+      clearForm () {
+        this.$v.$reset()
+        this.form.userName = null
+        this.form.lastName = null
+        this.form.age = null
+      },
+      saveUser () {
+        this.sending = true
+
+        // Instead of this timeout, here you can call your API
+        window.setTimeout(() => {
+          this.lastUser = `${this.form.firstName} ${this.form.lastName}`
+          this.userSaved = true
+          this.sending = false
+          this.clearForm()
+        }, 1500)
+      },
+      validateUser () {
+        this.$v.$touch()
+
+        if (!this.$v.$invalid) {
+          this.saveUser()
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .md-progress-bar {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+  }
+</style>
