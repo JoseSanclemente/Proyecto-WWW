c98159644b376bea14c2dc6219daf6e50fe11ea5
sofia <dnavas@truora.com>
2020-05-09 22:49:46 -0500

    add support for processing bank files and register payments

--- back/server/handler/bill.go
+++ back/server/handler/bill.go
@@ -3,24 +3,28 @@ package handler
 import (
 	"Proyecto-WWW/back/models/bill"
 	"Proyecto-WWW/back/shared/gateway"
+	"Proyecto-WWW/back/shared/random"
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
-func registerPayment(response http.ResponseWriter, request *http.Request) {
+func registerOperatorPayment(response http.ResponseWriter, request *http.Request) {
 	params := map[string]string{}
 
 	err := json.NewDecoder(request.Body).Decode(&params)
 	if err != nil {
-		fmt.Println("registerPayment_1: ", err.Error())
+		fmt.Println("registerOperatorPayment_1: ", err.Error())
 		gateway.WriteInternalServerError(response)
 		return
 	}
 
 	b, err := bill.Load(params["id"])
 	if err != nil {
-		fmt.Println("registerPayment_2: ", err.Error())
+		fmt.Println("registerOperatorPayment_2: ", err.Error())
 		gateway.WriteInternalServerError(response)
 		return
 	}
@@ -32,7 +36,7 @@ func registerPayment(response http.ResponseWriter, request *http.Request) {
 
 	err = b.RegisterPayment()
 	if err != nil {
-		fmt.Println("registerPayment_3: ", err.Error())
+		fmt.Println("registerOperatorPayment_3: ", err.Error())
 		gateway.WriteInternalServerError(response)
 		return
 	}
@@ -58,4 +62,82 @@ func createBill(response http.ResponseWriter, request *http.Request) {
 	}
 
 	gateway.WriteJSON(response, 200, map[string]string{"id": id})
+}
+
+func readBankFile(request *http.Request) ([][]string, error) {
+	err := request.ParseMultipartForm(256)
+	if err != nil {
+		fmt.Println("readBankFile_1: ", err.Error())
+		return nil, err
+	}
+
+	file, _, err := request.FormFile("file")
+	if err != nil {
+		fmt.Println("readBankFile_2: ", err.Error())
+		return nil, err
+	}
+	defer file.Close()
+
+	filename := random.GenerateID("FIL")
+
+	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println("readBankFile_3: ", err.Error())
+		return nil, err
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, file)
+	if err != nil {
+		fmt.Println("readBankFile_4: ", err.Error())
+		return nil, err
+	}
+
+	readFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("readBankFile_5: ", err.Error())
+		return nil, err
+	}
+
+	r := csv.NewReader(readFile)
+
+	records, err := r.ReadAll()
+	if err != nil {
+		fmt.Println("readBankFile_5: ", err.Error())
+		return nil, err
+	}
+
+	return records, nil
+}
+
+func registerBankPayments(response http.ResponseWriter, request *http.Request) {
+	rows, err := readBankFile(request)
+	if err != nil {
+		fmt.Println("registerBankPayments_1: ", err.Error())
+		gateway.WriteInternalServerError(response)
+		return
+	}
+
+	for _, row := range rows {
+		b, err := bill.Load(row[0])
+		if err != nil {
+			fmt.Println("registerBankPayments_2: ", err.Error())
+			gateway.WriteInternalServerError(response)
+			return
+		}
+
+		if b == nil {
+			gateway.WriteJSON(response, 400, map[string]string{"error": "bill not found"})
+			return
+		}
+
+		err = b.RegisterPayment()
+		if err != nil {
+			fmt.Println("registerBankPayments_3: ", err.Error())
+			gateway.WriteInternalServerError(response)
+			return
+		}
+	}
+
+	gateway.WriteJSON(response, 200, map[string]string{"message": "ok"})
 }
